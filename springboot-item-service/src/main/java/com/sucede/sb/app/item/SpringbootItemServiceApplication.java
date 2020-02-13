a17f32bd1467ff8d1b7e59d7a7441b2616e89896
Daniel Fernandez <dfernandez@certiorem.com>
2020-02-13 18:08:52 +0100

    Incluimos ribbon para balanceo de carga

--- springboot-item-service/src/main/java/com/sucede/sb/app/item/SpringbootItemServiceApplication.java
+++ springboot-item-service/src/main/java/com/sucede/sb/app/item/SpringbootItemServiceApplication.java
@@ -2,8 +2,10 @@ package com.sucede.sb.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RibbonClient(name = "products-service")
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootItemServiceApplication {
--- springboot-item-service/src/main/java/com/sucede/sb/app/item/clients/IProductClientRest.java
+++ springboot-item-service/src/main/java/com/sucede/sb/app/item/clients/IProductClientRest.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.sucede.sb.app.item.models.Product;
 
 /*spring.application.name del Servicio Productos*/
-@FeignClient(name = "products-service", url = "localhost:8001")
+/*sin robbon*/
+//@FeignClient(name = "products-service", url = "localhost:8001")
+/*con ribbon no necesitamos la url y el puerto. Lo hacemos en el application.properties*/
+@FeignClient(name = "products-service")
 public interface IProductClientRest {
 
 	@GetMapping("/list")
