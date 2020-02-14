9a39b2a8c29666ad6f79a6d912d13a357f639523
Daniel Fernandez <dfernandez@certiorem.com>
2020-02-14 15:14:33 +0100

    Habilitamos Hystrix y habilitamos CircuitBreaker para tolerancia a fallos en las llamadas rest

--- springboot-item-service/src/main/java/com/sucede/sb/app/item/SpringbootItemServiceApplication.java
+++ springboot-item-service/src/main/java/com/sucede/sb/app/item/SpringbootItemServiceApplication.java
@@ -2,9 +2,11 @@ package com.sucede.sb.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 //La anotacion de RibbonClient sobra despues de incluir Eureka
 //@RibbonClient(name = "products-service")
--- springboot-item-service/src/main/java/com/sucede/sb/app/item/controllers/ItemController.java
+++ springboot-item-service/src/main/java/com/sucede/sb/app/item/controllers/ItemController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sucede.sb.app.item.models.Item;
+import com.sucede.sb.app.item.models.Product;
 import com.sucede.sb.app.item.models.service.ItemService;
 
 @RestController
@@ -23,8 +25,24 @@ public class ItemController {
 		return itemService.findAll();
 	}
 	
+	/* Con esta anotación en caso de fallo, irá al método registrado 
+	 * en fallbackMethod*/
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/view/{id}/quantity/{quantity}")
 	public Item detail(@PathVariable Long id, @PathVariable Integer quantity) {
 		return itemService.findById(id, quantity);
+	}
+	
+	public Item metodoAlternativo (Long id, Integer quantity) {
+		Item item = new Item();
+		Product product = new Product();
+		
+		item.setQuantity(quantity);
+		product.setId(id);
+		product.setNombre("Sony Camera");
+		product.setPrecio(725.99);
+		item.setProduct(product);
+		
+		return item;
 	}
 }
