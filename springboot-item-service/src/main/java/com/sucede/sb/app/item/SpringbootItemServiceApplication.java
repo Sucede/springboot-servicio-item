b68ec012c0f091d0f7869d0383cf2df2ff0d9627
Daniel Fernandez <dfernandez@certiorem.com>
2020-02-14 13:07:04 +0100

    Establecemos el servicio como cliente Eureka y quitamos ribbon

--- springboot-item-service/src/main/java/com/sucede/sb/app/item/SpringbootItemServiceApplication.java
+++ springboot-item-service/src/main/java/com/sucede/sb/app/item/SpringbootItemServiceApplication.java
@@ -2,10 +2,12 @@ package com.sucede.sb.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "products-service")
+@EnableEurekaClient
+//La anotacion de RibbonClient sobra despues de incluir Eureka
+//@RibbonClient(name = "products-service")
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootItemServiceApplication {
